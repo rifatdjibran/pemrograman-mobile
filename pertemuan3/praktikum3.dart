e5accea0d87970986b8e81dcccc1b17eaa631578
rifat djibran <rifatdjibran099@gmail.com>
2026-03-04 19:30:12 +0700

    Pertemuan 3

--- pertemuan3/praktikum3.dart
+++ pertemuan3/praktikum3.dart
@@ -1,18 +1,28 @@
 void main() {
 
-for (int index = 10; index < 27; index++) {
-  print(index);
-}
+  // ======================
+  // LANGKAH 1 & 2 
+  // ======================
 
-for (int index = 10; index < 27; index++) {
-
-  if (index == 21) {
-    break;
-  } else if (index > 1 && index < 7) {
-    continue;
+  for (int index = 10; index < 27; index++) {
+    print(index);
   }
 
-  print(index);
-}
+  print("===== LANGKAH 3 =====");
+
+  // ======================
+  // LANGKAH 3 
+  // ======================
+
+  for (int index = 10; index < 27; index++) {
+
+    if (index == 21) {
+      break;
+    } else if (index > 1 && index < 7) {
+      continue;
+    }
+
+    print(index);
+  }
 
 }
